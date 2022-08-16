926e57fac1ad82778425312d4a5cb65ca117bab4
mjjstockman <mjjstockman@gmail.com>
2022-08-16 11:18:37 +0000

    Add search functionality

--- products/views.py
+++ products/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+# Q helps handle queries
+from django.db.models import Q
 from .models import Product
 
 
@@ -8,9 +11,30 @@ def all_products(request):
     """
 
     products = Product.objects.all()
+    # set query as None so no error when no adding empty query to context
+    query = None
+
+    # check if request.get exists (if get param was sent in request from search form in nav)
+    if request.GET:
+        # form text input was named q, so check if q is in request.GET
+        if 'q' in request.GET:
+            # if so rename q as query var
+            query = request.GET['q']
+            # if search box was blank q will not be in request.GET
+            if not query:
+                # use django messages framework to add error msg to request
+                messages.error(request, "You didn't enter any search criteria!")
+                # redirect to products url
+                return redirect(reverse('products'))
+
+            # set queries var to Q obj where name or description contains the query, case insensitive
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            # use filter method with queries var passed to it
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)

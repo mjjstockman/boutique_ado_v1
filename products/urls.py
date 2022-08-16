f0246cc92de8dd2fd3893893be2deedfc6ee9c58
mjjstockman <mjjstockman@gmail.com>
2022-08-16 09:38:50 +0000

    Add product details functionality

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
+    # use angle brackets for dynamic info
+    path('<product_id>', views.product_detail, name='product_detail'),
 ]

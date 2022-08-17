9e328ec5c51d755cc0dcd919869e0471b7f85946
mjjstockman <mjjstockman@gmail.com>
2022-08-17 18:40:18 +0000

    Update contect processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 # context processor, makes context dict available to all templates
 # must add to templates context_processors in settings
@@ -8,6 +10,20 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    # if bag exists in session get it, if not make bag var as an empty dict
+    bag = request.session.get('bag', {})
+
+    # loop through the bag in the session
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        # add dict with bag info to bag_items
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)

5c46d8ed9458d1ae23b7da45c88da193224015fc
mjjstockman <mjjstockman@gmail.com>
2022-08-21 09:33:44 +0000

    Add stripe elements

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,36 @@
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
+
+// get the keys, slice off the quotation
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+var client_secret = $('#id_client_secret').text().slice(1, -1);
+// create var using stripe key
+var stripe = Stripe(stripe_public_key);
+// create instance of stripe elements
+var elements = stripe.elements();
+// add styles (from stripe js docs)
+var style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+//  create card element and add the style
+var card = elements.create('card', {
+    style: style
+});
+// mount card to the div
+card.mount('#card-element');

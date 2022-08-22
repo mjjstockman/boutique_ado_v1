1b1b51a0673803c4702f418dab110ff0085bc276
mjjstockman <mjjstockman@gmail.com>
2022-08-22 08:52:14 +0000

    Add order summary too checkout success page

--- checkout/signals.py
+++ checkout/signals.py
@@ -15,5 +15,5 @@ def update_on_delete(sender, instance, **kwargs):
     """
     Update order total on lineitem delete
     """
-    print('delete dignal recieved!')
+    print('delete signal recieved!')
     instance.order.update_total()
